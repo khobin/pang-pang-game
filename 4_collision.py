ed0282ecbfca8ac229e3733fad9c5cfc18148b00
hobin <gatobin24@naver.com>
2022-06-03 21:31:10 +0900

    5_ball_division

--- 4_collision.py
+++ 4_collision.py
@@ -7,9 +7,9 @@ pygame.init()
 # 화면 크기 설정
 screen_width = 640
 screen_height = 480
-screen = pygame.display.set_mode((screen_width,screen_height))
+screen = pygame.display.set_mode((screen_width, screen_height))
 
-# Title 
+# Title
 pygame.display.set_caption("PANG PANG")
 
 # FPS
@@ -18,8 +18,8 @@ clock = pygame.time.Clock()
 
 # 1. 사용자 게임 초기화 (배경화면, 게임 이미지, 좌표, 속도, 폰트 등)
 
-current_path = os.path.dirname(__file__) # 현재 파일 위치 반환
-image_path = os.path.join(current_path, "images") # image 폴더 위치 반환
+current_path = os.path.dirname(__file__)  # 현재 파일 위치 반환
+image_path = os.path.join(current_path, "images")  # image 폴더 위치 반환
 
 # 배경 만들기
 background = pygame.image.load(os.path.join(image_path, "background.png"))
@@ -31,7 +31,7 @@ stage_height = stage_size[1]
 
 # 캐릭터 만들기
 character = pygame.image.load(os.path.join(image_path, "character.png"))
-character_size =character.get_rect().size
+character_size = character.get_rect().size
 character_width = character_size[0]
 character_height = character_size[1]
 character_x_pos = (screen_width / 2) - (character_width / 2)
@@ -55,44 +55,49 @@ weapons = []
 weapon_speed = 10
 
 # 공 만들기(4 개 크기 따로 처리)
-ball_images = [ 
+ball_images = [
     pygame.image.load(os.path.join(image_path, "balloon1.png")),
     pygame.image.load(os.path.join(image_path, "balloon2.png")),
     pygame.image.load(os.path.join(image_path, "balloon3.png")),
     pygame.image.load(os.path.join(image_path, "balloon4.png"))]
 
 # 공이 클수록 속도가 빠름
-ball_speed_y = [-18, -15, -12, -9] 
+ball_speed_y = [-18, -15, -12, -9]
 
 # 공들
 balls = []
 
 balls.append({
-    "pos_x" : 50, # 공의 x좌표
-    "pos_y" : 50, # 공의 y좌표
-    "img_idx" : 0, # 공의 이미지 인덱스
-    "to_x" : 3, # x 축 이동방향, -3 이면 왼쪽으로, 3 이면 오른쪽으로
-    "to_y" : -6, # y 축 이동방향
-    "init_spd_y" :ball_speed_y[0] # y 최초 속도
-    })
+    "pos_x": 50,  # 공의 x좌표
+    "pos_y": 50,  # 공의 y좌표
+    "img_idx": 0,  # 공의 이미지 인덱스
+    "to_x": 3,  # x 축 이동방향, -3 이면 왼쪽으로, 3 이면 오른쪽으로
+    "to_y": -6,  # y 축 이동방향
+    "init_spd_y": ball_speed_y[0]  # y 최초 속도
+})
+
+# 사라질 무기, 공 정보 저장 변수
+weapon_to_remove = -1
+ball_to_remove = -1
 
 # Event loop
 running = True
 while running:
-    
-    dt = clock.tick(30) # 게임 화면 초당 프레임 수
+
+    dt = clock.tick(30)  # 게임 화면 초당 프레임 수
     # 2. 이벤트 처리 (키보드, 마우스 등)
     for event in pygame.event.get():  # 어떤 이벤트 발생하였는지
         if event.type == pygame.QUIT:
             running = False
-        
+
         if event.type == pygame.KEYDOWN:
             if event.key == pygame.K_LEFT:
                 character_to_x -= character_speed
             elif event.key == pygame.K_RIGHT:
                 character_to_x += character_speed
-            elif event.key == pygame.K_SPACE: # 무기 발사
-                weapon_x_pos = character_x_pos + (character_width / 2) - (weapon_width / 2) 
+            elif event.key == pygame.K_SPACE:  # 무기 발사
+                weapon_x_pos = character_x_pos + \
+                    (character_width / 2) - (weapon_width / 2)
                 weapon_y_pos = character_y_pos
                 weapons.append([weapon_x_pos, weapon_y_pos])
 
@@ -103,17 +108,17 @@ while running:
     # 3. 게임 캐릭터 위치 정의
 
     character_x_pos += character_to_x
-    
+
     if character_x_pos < 0:
         character_x_pos = 0
     elif character_x_pos > screen_width - character_width:
         character_x_pos = screen_width - character_width
 
     # 무기 위치 조정
-    weapons = [ [w[0], w[1] - weapon_speed] for w in weapons] # 무기 위치를 위로 올림
-    
+    weapons = [[w[0], w[1] - weapon_speed] for w in weapons]  # 무기 위치를 위로 올림
+
     # 천장에 닿은 무기 없애기
-    weapons = [ [w[0], w[1]] for w in weapons if w[1] > 0]
+    weapons = [[w[0], w[1]] for w in weapons if w[1] > 0]
 
     # 공 위치 정의
     for ball_idx, ball_val in enumerate(balls):
@@ -132,16 +137,61 @@ while running:
         # 스테이지에 튕겨서 올라가는 처리
         if ball_pos_y >= screen_height - stage_height - ball_height:
             ball_val["to_y"] = ball_val["init_spd_y"]
-        else : # 그 외의 모든 경우에는 속도를 줄임
+        else:  # 그 외의 모든 경우에는 속도를 줄임
             ball_val["to_y"] += 0.5
 
         ball_val["pos_x"] += ball_val["to_x"]
         ball_val["pos_y"] += ball_val["to_y"]
-    # 4. 충돌 처리
 
-    # 5. 화면 그리기
-    screen.blit(background, (0,0))  # 배경 그리기
+    # 4. 충돌 처리
     
+    # 캐릭터 rect 정보 업데이트
+    character_rect = character.get_rect()
+    character_rect.left = character_x_pos
+    character_rect.top = character_y_pos
+
+    for ball_idx, ball_val in enumerate(balls):
+        ball_pos_x = ball_val["pos_x"]
+        ball_pos_y = ball_val["pos_y"]
+        ball_img_idx = ball_val["img_idx"]
+
+        # 공 rect 정보 업데이트
+        ball_rect = ball_images[ball_img_idx].get_rect()
+        ball_rect.left = ball_pos_x
+        ball_rect.top = ball_pos_y
+
+        # 공과 캐릭터 충돌 체크
+        if character_rect.colliderect(ball_rect):
+            running = False
+            break
+        
+        # 공과 무기들 충돌 처리
+        for weapon_idx, weapon_val in enumerate(weapons):
+            weapon_pos_x = weapon_val[0]
+            weapon_pos_y = weapon_val[1]
+
+            # 무기 rect 정보 업데이트
+            weapon_rect = weapon.get_rect()
+            weapon_rect.left = weapon_pos_x
+            weapon_rect.top = weapon_pos_y
+
+            # 충돌 체크
+            if weapon_rect.colliderect(ball_rect):
+                weapon_to_remove = weapon_idx
+                ball_to_remove = ball_idx
+                break
+
+    # 충돌된 공 and 무기 없애기
+    if ball_to_remove > -1:
+        del balls[ball_to_remove]        
+        ball_to_remove = -1
+    if weapon_to_remove > -1:
+        del weapons[weapon_to_remove]
+        weapon_to_remove = -1
+    
+    # 5. 화면 그리기
+    screen.blit(background, (0, 0))  # 배경 그리기
+
     for weapon_x_pos, weapon_y_pos in weapons:
         screen.blit(weapon, (weapon_x_pos, weapon_y_pos))
     for idx, val in enumerate(balls):
@@ -150,15 +200,10 @@ while running:
         ball_img_idx = val["img_idx"]
         screen.blit(ball_images[ball_img_idx], (ball_pos_x, ball_pos_y))
 
-    screen.blit(stage, (0, screen_height - stage_height))    
+    screen.blit(stage, (0, screen_height - stage_height))
     screen.blit(character, (character_x_pos, character_y_pos))
-    
-    
 
- 
-
-    pygame.display.update() # 게임화면을 매 프레임마다 다시 그리기
-
+    pygame.display.update()  # 게임화면을 매 프레임마다 다시 그리기
 
 
 pygame.quit
